8ba38cbd6d798ea86175e19f942cc80f7f207c80
Alexandre <buissonalexandre16@gmail.com>
2018-11-21 15:23:17 +0100

    Implémentation de sass

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeInputs from './components/HomeInputs';
+
+const App = () => (
+  <div>
+    <div className="content" />
+    <HomeInputs />
+  </div>
+);
+
+export default App;
