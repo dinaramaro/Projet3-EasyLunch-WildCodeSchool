6edbfcc541d29637410f9b209c4776294c921d98
MickaelMuller <Muller-Mickael@live.fr>
2018-11-22 19:41:16 +0100

    modif ordre explication

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import YouTube from 'react-youtube';
+import './Media.css';
 
 const Media = () => {
   const opts = {
-    height: '350',
-    width: '100%',
     playerVars: { // différente configuration de lecture, d'affichage, d'utilisation//
       autoplay: 0,
       start: 120,
@@ -14,11 +13,10 @@ const Media = () => {
       showinfo: 0,
       modestbranding: 1,
     },
-
   };
-
   return (
     <YouTube
+      className="video"
       videoId="RluXbJ4CZHU" // Lien id de la video, pour inserer une video, tu as le choix soit avec replace ou slice//
       opts={opts}
     />
