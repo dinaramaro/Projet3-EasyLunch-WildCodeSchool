4d3f9ba1160e026e9e2f13001f5beaa4c49eb05e
MickaelMuller <Muller-Mickael@live.fr>
2018-11-23 18:05:17 +0100

    modif partner

--- src/components/about/Partners.jsx
+++ src/components/about/Partners.jsx
@@ -1,50 +1,69 @@
 import React from 'react';
 import {
-  Col, Row, Card, CardImg, CardText, CardBody, Container,
+  Col,
+  Row,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  Container,
 } from 'reactstrap';
 import './Partners.scss';
 
-const Partenaires = () => (
-  <Container>
-    <section className="Partners">
-      <h1 className="title">NOS PARTENAIRES</h1>
-      <Row>
-        <Col lg="4" sm="12" xs="12">
-          <Card>
-            <CardImg top src="/medias/SQAL.png" alt="sqal" className="img-fluid" />
-            <CardBody>
-              <CardText>
-                {`SQAL est une agence spécialisée dans l’innovation. C’est elle qui a développé l’application mobile Easy Lunch. 
-                L’équipe est efficace, réactive et disponible. Elle s’adapte aux jeunes start-up comme la nôtre. Vous trouverez plus d’informations sur leur site Internet.`}
-              </CardText>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col lg="4" sm="12" xs="12">
-          <Card>
-            <CardImg top src="/medias/Wild-Code-School.png" alt="wild-code-school" className="img-fluid" />
-            <CardBody>
-              <CardText>
-                {`La Wild Code School forme de futurs développeurs web grâce à une pédagogie innovante et des outils numériques permettant aux élèves de mettre en pratique les enseignements. 
-                Sept étudiants ont réalisé notre site web dans le cadre d'un projet d'école.`}
-              </CardText>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col lg="4" sm="12" xs="12">
-          <Card>
-            <CardImg top className="img-fluid" src="/medias/logo-nouvelle-aquitaine.png" alt="region-nouvelle-aquitaine" />
-            <CardBody>
-              <CardText>
-                {`LoremIpsum..... Code School forme de futurs développeurs web grâce à une pédagogie innovante et des outils numériques permettant aux élèves de mettre en pratique les enseignements. 
-                Sept étudiants ont réalisé notre site web dans le cadre d'un projet d'école.`}
-              </CardText>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </section>
+const Partners = () => (
+  <Container className="Partners">
+    <h1 className="title">NOS PARTENAIRES</h1>
+    <Row>
+      <Col lg="4" sm="12" xs="12">
+        <Card>
+          <CardImg
+            top
+            src="/medias/SQAL.png"
+            alt="sqal"
+            className="img-fluid"
+          />
+          <CardBody>
+            <CardText>
+              {`SQAL est une agence spécialisée dans l’innovation. C’est elle qui a développé l’application mobile Easy Lunch. 
+              L’équipe est efficace, réactive et disponible. Elle s’adapte aux jeunes start-up comme la nôtre. Vous trouverez plus d’informations sur leur site Internet.`}
+            </CardText>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col lg="4" sm="12" xs="12">
+        <Card>
+          <CardImg
+            top
+            src="/medias/Wild-Code-School.png"
+            alt="wild-code-school"
+            className="img-fluid"
+          />
+          <CardBody>
+            <CardText>
+              {`La Wild Code School forme de futurs développeurs web grâce à une pédagogie innovante et des outils numériques permettant aux élèves de mettre en pratique les enseignements. 
+              Sept étudiants ont réalisé notre site web dans le cadre d'un projet d'école.`}
+            </CardText>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col lg="4" sm="12" xs="12">
+        <Card>
+          <CardImg
+            top
+            className="img-fluid"
+            src="/medias/logo-nouvelle-aquitaine.png"
+            alt="region-nouvelle-aquitaine"
+          />
+          <CardBody>
+            <CardText>
+              {`LoremIpsum..... Code School forme de futurs développeurs web grâce à une pédagogie innovante et des outils numériques permettant aux élèves de mettre en pratique les enseignements. 
+              Sept étudiants ont réalisé notre site web dans le cadre d'un projet d'école.`}
+            </CardText>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   </Container>
 );
 
-export default Partenaires;
+export default Partners;
