0b171c1c778efca88b3311dc8317a4a22503e7a7
MickaelMuller <Muller-Mickael@live.fr>
2018-12-11 12:30:46 +0100

    on submit form

--- front/src/components/home/HomeInputs.jsx
+++ front/src/components/home/HomeInputs.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import './HomeInputs.scss';
-import { Input, Button } from 'reactstrap';
+import { Input, Button, Form } from 'reactstrap';
 import { withRouter } from 'react-router';
 
 const imgSrc = 'medias/hero-image.png';
-
 
 class HomeInputs extends Component {
   constructor(props) {
@@ -32,42 +31,46 @@ class HomeInputs extends Component {
     const { keyword, personCapacity } = this.state;
     return (
       <div className="HomeInputs">
-        <div
-          className="background"
-          style={{ backgroundImage: `url(${imgSrc})`, height: '34vw' }}
-        >
-          <h2 className="border-text banner-text">
-            Commandez et payez avant et soyez servis dès votre arrivée au restaurant
-          </h2>
-          <Input
-            className="search1"
-            placeholder="Restaurant"
-            style={{ width: '25vw' }}
-            value={keyword}
-            onChange={this.onChange}
-            name="keyword"
-          />
-          <Input
-            className="search2"
-            type="select"
-            placeholder="Nombre de personnes"
-            style={{ width: '25vw' }}
-            name="personCapacity"
-            value={personCapacity}
-            onChange={this.onChange}
+        <Form onSubmit={() => this.pageResult()}>
+          <div
+            className="background"
+            style={{ backgroundImage: `url(${imgSrc})`, height: '34vw' }}
           >
-            <option>Pour combien ?</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-          </Input>
-          <Button onClick={() => this.pageResult(keyword, personCapacity)} className="search-button btn-submit">Rechercher</Button>
-          <Button className="participe-button">Je participe</Button>
-        </div>
+            <h2 className="border-text banner-text">
+              Commandez et payez avant et soyez servis dès votre arrivée au
+              restaurant
+            </h2>
+
+            <Input
+              className="search1"
+              placeholder="Restaurant"
+              style={{ width: '25vw' }}
+              value={keyword}
+              onChange={this.onChange}
+              name="keyword"
+            />
+            <Input
+              className="search2"
+              type="select"
+              placeholder="Nombre de personnes"
+              style={{ width: '25vw' }}
+              name="personCapacity"
+              value={personCapacity}
+              onChange={this.onChange}
+            >
+              <option>Pour combien ?</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+            </Input>
+            <Button className="search-button btn-submit">Rechercher</Button>
+            <Button className="participe-button">Je participe</Button>
+          </div>
+        </Form>
       </div>
     );
   }
--- front/src/components/result/Restaurants.jsx
+++ front/src/components/result/Restaurants.jsx
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {
-  Row, Col, Input, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button,
+  Row,
+  Col,
+  Input,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Form,
 } from 'reactstrap';
 import './Restaurants.scss';
 import { withRouter } from 'react-router';
 import { varServeur } from '../../constants';
 import { dataResults } from '../../actions/search';
 
-
 class Restaurants extends Component {
   constructor(props) {
     super(props);
-    const { location: { search } } = this.props;
+    const {
+      location: { search },
+    } = this.props;
     this.state = {
-      keyword: search.replace('?keyword=', '').replace('&personcapacity', '').replace(/[^]\d/, ''),
+      keyword: search
+        .replace('?keyword=', '')
+        .replace('&personcapacity', '')
+        .replace(/[^]\d/, ''),
       personCapacity: search.replace(/\D+/g, ''),
     };
     this.onChange = this.onChange.bind(this);
@@ -29,57 +42,69 @@ class Restaurants extends Component {
     });
   }
 
-  searchSubmit() {
+  searchSubmit(e) {
+    e.preventDefault();
     const { keyword, personCapacity } = this.state;
     const { resultRestaurants } = this.props;
     resultRestaurants(`${varServeur}search/?keyword=${keyword}&personcapacity=${personCapacity}`);
   }
-
 
   render() {
     const { searchResults: { results } } = this.props;
     const { keyword, personCapacity } = this.state;
     return (
       <div className="Restaurants">
-        <Row>
-          <Col xs="4" className="padding">
-            <Input
-              className="search1"
-              placeholder="Restaurant"
-              value={keyword}
-              onChange={this.onChange}
-              name="keyword"
-            />
-          </Col>
-          <Col xs="3">
-            <Input
-              type="select"
-              className="search2"
-              placeholder="Nombre de personnes"
-              value={personCapacity}
-              onChange={this.onChange}
-              name="personCapacity"
-
-            >
-              <option>Pour combien ?</option>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-            </Input>
-          </Col>
-          <Col xs="4">
-            <Button onClick={() => this.searchSubmit()} color="warning" className="submitButton all-btn">Rechercher</Button>
-          </Col>
-        </Row>
+        <Form onSubmit={this.searchSubmit}>
+          <Row>
+            <Col xs="4" className="padding">
+              <Input
+                className="search1"
+                placeholder="Restaurant"
+                value={keyword}
+                onChange={this.onChange}
+                name="keyword"
+              />
+            </Col>
+            <Col xs="3">
+              <Input
+                type="select"
+                className="search2"
+                placeholder="Nombre de personnes"
+                value={personCapacity}
+                onChange={this.onChange}
+                name="personCapacity"
+              >
+                <option>Pour combien ?</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+              </Input>
+            </Col>
+            <Col xs="4">
+              <Button
+                color="warning"
+                className="btn-submit submit-button all-btn"
+              >
+                Rechercher
+              </Button>
+            </Col>
+          </Row>
+        </Form>
         <Row>
           {results.map(item => (
             <Col sm="12" md="6" xl="4">
               <Card className="card-restaurant">
-                <CardImg top width="400px" height="175px" src={item.picture} alt="Card image cap" />
+                <CardImg
+                  top
+                  width="400px"
+                  height="175px"
+                  src={item.picture}
+                  alt="Card image cap"
+                />
                 <CardBody>
                   <CardTitle>{item.name}</CardTitle>
                   <CardSubtitle>{item.address}</CardSubtitle>
@@ -104,4 +129,9 @@ function mdtp(dispatch) {
   return bindActionCreators({ resultRestaurants: dataResults }, dispatch);
 }
 
-export default withRouter(connect(mstp, mdtp)(Restaurants));
+export default withRouter(
+  connect(
+    mstp,
+    mdtp,
+  )(Restaurants),
+);
