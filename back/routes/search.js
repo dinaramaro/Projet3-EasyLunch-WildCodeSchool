ea503163eaa6274f73be1c7b5c300e7f85973b5d
MickaelMuller <Muller-Mickael@live.fr>
2019-01-11 09:24:10 +0100

    route multi ok

--- back/routes/search.js
+++ back/routes/search.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const keyword = `%${req.query.keyword}%`;
   const personcapacity = req.query.personcapacity;
-  connection.query('SELECT DISTINCT id, name, description, lat, lon, address, picture, city, person_capacity FROM public_restaurants WHERE name LIKE ? AND person_capacity >= ?', [keyword, personcapacity], (err, results) => {
+  connection.query('SELECT DISTINCT id, name, description, lat, lon, address, picture, city, person_capacity FROM public_restaurants WHERE name LIKE ? OR address like ? OR city LIKE ? OR description LIKE ? AND person_capacity >= ?', [keyword, keyword, keyword, keyword, personcapacity], (err, results) => {
     if (err) {
-      console.log(err);
+      res.sendStatus(500);
       res.status(500).send('Erreur');
     } else {
       res.json(results);
