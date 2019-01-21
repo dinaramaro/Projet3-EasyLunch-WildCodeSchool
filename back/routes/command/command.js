b1dabf8350900289dae664d9dd05a925691bb8ce
MickaelMuller <Muller-Mickael@live.fr>
2019-01-21 21:06:15 +0100

    id_custom ok

--- back/routes/command/command.js
+++ back/routes/command/command.js
@@ -25,68 +25,79 @@ router.post('/', (req, res) => {
             if (err3) {
               res.status(500).send(`Erreur public booking: ${err3}`);
             } else {
-              const bookingId = results3.insertId;
-              const { tableCommand } = req.body;
-              const newCommand = {
-                ...tableCommand,
-                booking_id: bookingId,
-              };
-              connection.query('INSERT INTO public_command SET ?', newCommand, (err4, results4) => {
+              const { tableCommand, tableBooking: { restaurant_id } } = req.body;
+              connection.query('SELECT cmd.id FROM public_command cmd JOIN public_booking book ON cmd.booking_id = book.id WHERE book.restaurant_id = ?', restaurant_id, (err4, results4) => {
                 if (err4) {
-                  res.status(500).send(`Erreur public command: ${err4}`);
+                  res.sendStatus(500);
                 } else {
-                  const commandId = results4.insertId;
-                  const { tablePayment, idStripe } = req.body;
-                  const newPayment = {
-                    ...tablePayment,
-                    command_id: commandId,
-                    stripe_id: idStripe,
+                  const year = new Date().getFullYear();
+                  const month = new Date().getMonth();
+                  const id_custom = `${restaurant_id}-${year}-${month + 1}-${results4.length}`;
+                  const bookingId = results3.insertId;
+                  const newCommand = {
+                    ...tableCommand,
+                    booking_id: bookingId,
+                    id_custom,
                   };
-                  connection.query('INSERT INTO public_payment SET ?', newPayment, (err5, results5) => {
+                  connection.query('INSERT INTO public_command SET ?', newCommand, (err5, results5) => {
                     if (err5) {
-                      res.status(500).send(`Erreur public payment: ${err5}`);
+                      res.status(500).send(`Erreur public command: ${err5}`);
                     } else {
-                      const paymentId = results5.insertId;
-                      connection.query('UPDATE public_command SET payment_id = ? WHERE id = ?', [paymentId, commandId], (err6) => {
+                      const commandId = results5.insertId;
+                      const { tablePayment, idStripe } = req.body;
+                      const newPayment = {
+                        ...tablePayment,
+                        command_id: commandId,
+                        stripe_id: idStripe,
+                      };
+                      connection.query('INSERT INTO public_payment SET ?', newPayment, (err6, results6) => {
                         if (err6) {
-                          res.sendStatus(500);
+                          res.status(500).send(`Erreur public payment: ${err6}`);
                         } else {
-                          connection.query('SELECT name, mail FROM public_users_app JOIN public_booking ON public_users_app.id = public_booking.master_user_id WHERE public_booking.id = ?', bookingId, (err7, results7) => {
+                          const paymentId = results6.insertId;
+                          connection.query('UPDATE public_command SET payment_id = ? WHERE id = ?', [paymentId, commandId], (err7) => {
                             if (err7) {
                               res.sendStatus(500);
-                            } else {                              
-                              const { name, mail } = results7[0];
-                              const output = `
-                            <h1>Merci ${name} d'avoir commandé chez EASYLUNCH</h1>
-                            <br />
-                            <p>Votre commande a bien été réservée, bon appétit!</p>
-                            <br />
-                            <p> Je vous remercie de votre commande</p>
-                          `;
-                              const transporter = nodemailer.createTransport({
-                                host: 'smtp.gmail.com',
-                                port: 465,
-                                secure: true,
-                                auth: {
-                                  user: senderMail,
-                                  pass: passwordMail,
-                                },
-                              });
-                              const mailOptions = {
-                                from: `EasyLunch Commande ${senderMail}`,
-                                to: mail,
-                                subject: 'Votre commande chez EASYLUNCH',
-                                html: output
-                              };
-                              transporter.sendMail(mailOptions, (error) => {
-                                if (error) {
+                            } else {
+                              connection.query('SELECT name, mail FROM public_users_app JOIN public_booking ON public_users_app.id = public_booking.master_user_id WHERE public_booking.id = ?', bookingId, (err8, results8) => {
+                                if (err8) {
                                   res.sendStatus(500);
+                                } else {
+                                  const { name, mail } = results8[0];
+                                  const output = `
+                                <h1>Merci ${name} d'avoir commandé chez EASYLUNCH</h1>
+                                <br />
+                                <p>Votre commande a bien été réservée, bon appétit!</p>
+                                <br />
+                                <p> Votre numéro de commande est le ${id_custom}.
+                                <p> Je vous remercie de votre commande</p>
+                              `;
+                                  const transporter = nodemailer.createTransport({
+                                    host: 'smtp.gmail.com',
+                                    port: 465,
+                                    secure: true,
+                                    auth: {
+                                      user: senderMail,
+                                      pass: passwordMail,
+                                    },
+                                  });
+                                  const mailOptions = {
+                                    from: `EasyLunch Commande ${senderMail}`,
+                                    to: mail,
+                                    subject: 'Votre commande chez EASYLUNCH',
+                                    html: output
+                                  };
+                                  transporter.sendMail(mailOptions, (err9) => {
+                                    if (err9) {
+                                      res.sendStatus(500);
+                                    }
+                                    res.sendStatus(200);
+                                  });
                                 }
-                                res.sendStatus(200);
                               });
+                              res.json(nameCode);
                             }
                           });
-                          res.json(nameCode);
                         }
                       });
                     }
